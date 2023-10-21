e5be210fa026a3f930aab50237fc848783f0bdfb
hajarasata <75947721+hajarasata@users.noreply.github.com>
2023-10-21 14:45:25 +0200

    update 211023

---
+++ forms/contact.php
@@ -0,0 +1,69 @@
+<?php
+  
+if($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $email_title = "";
+    $concerned_department = "";
+    $visitor_message = "";
+    $email_body = "<div>";
+      
+    if(isset($_POST['visitor_name'])) {
+        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+                        </div>";
+    }
+ 
+    if(isset($_POST['visitor_email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
+                        </div>";
+    }
+      ($_POST['visitor_message'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$visitor_message."</div>
+                        </div>";
+    
+	
+	}
+	
+	
+    
+  
+        $recipient = "hajarasata@gmail.com";
+    
+      
+    $email_body .= "</div>";
+ 
+    if(isset($_POST['email_title'])) {
+        $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email_title."</span>
+                        </div>";
+    }
+      
+ 
+    
+      
+    if(isset
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+      
+    if(mail($recipient, $email_title, $email_body, $headers)) {
+        echo "<p>Merci de nous avoir contacté, $visitor_name. Nous allons revenir vers vous rapidement.</p>";
+    } else {
+        echo '<p>Désolé, le message n\'a pas pu être envoyé.</p>';
+    }
+      
+} else {
+    echo '<p>Something went wrong</p>';
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,534 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+ <!-- mila esorina -->
+<meta name="robots" content="noindex">
+
+  <title>Auditeurs Associés Marseille</title>
+  <meta content="Cabinet d'expertise comptable à Marseille" name="description">
+
+
+  <!-- Favicons -->
+  <link href="assets/img/icone site.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+
+
+
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-between">
+
+		<a href="index.html" class="logo"><img src="assets/img/logo ordre.png" alt="" class="img-fluid"></a>
+
+		<a href="index.html" class="logo"><img src="assets/img/LOGO CNCC KELY.png" alt="" class="img-fluid"></a>
+
+
+	</div>
+	
+	
+	
+	 <div class="container d-flex align-items-center justify-content-between">
+
+      <nav id="navbar" class="navbar">
+        <ul>
+			<li><a class="nav-link scrollto active" href="#hero">Acceuil</a></li>
+          
+			
+			<li><a class="nav-link scrollto" href="#services">Nos services</a></li>
+
+
+
+			<li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+		  
+			<li class="dropdown"><a href="#"><span>Liens utiles</span> <i class="bi bi-chevron-down"></i></a>
+				<ul>
+				
+				  <li><a href="mentions.html">Mentions légales</a></li>
+				  <li><a href="https://annuaire.experts-comptables.org/expert-comptable/11029-auditeurs-associes-fiduciaire-conseils-marseille-13001" target="blank">Ordre des experts-comptables</a></li>
+				  <li><a href="https://www.urssaf.fr/portail/home.html" target="blank">Urssaf.fr</a></li>
+				  <li><a href="https://www.impots.gouv.fr/professionnel" target="blank">Impots.gouv.fr</a></li>
+				  
+				</ul>
+				
+			</li>
+			
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-end align-items-center">
+    <div class="hero-container">
+      <h3> <strong>Cabinet d'expertise comptable à Marseille</strong></h3>
+      <h1><a href="index.php" class="logo"><img src="assets/img/logo big.png" alt="" class="img-fluid"></a></h1>
+	  
+      <h2>Un accompagnement sur-mesure dans toutes les phases de votre activité professionnelle, 
+	  <br/>depuis la création jusqu'à la gestion comptable, fiscale, sociale et juridique.</h2>
+	  
+	  
+      <a href="#services" class="btn-get-started scrollto">Découvrir nos services</a>
+    </div>
+	
+	<!-- Waves effect -->
+  	    <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+      <defs>
+        <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+      </defs>
+      <g class="wave1">
+        <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+      </g>
+      <g class="wave2">
+        <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+      </g>
+      <g class="wave3">
+        <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+      </g>
+    </svg>
+	
+	
+  </section><!-- End Hero -->
+  
+  
+
+
+  <main id="main">
+
+
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <h2><a href="#services">Nos services selon votre activité</a></h2>
+          
+        </div>
+		 <div class="clients-slider swiper">
+		            <div class="swiper-wrapper align-items-center">
+
+        <div class="services-box">
+		
+		
+		          <div class="col-md-6 col-lg-2 d-flex align-items-stretch mb-5 mb-lg-0">
+				  
+				  
+							<div class="icon-box">
+								<div class="icon"><i class=""></i></div>
+							  
+							  <!-- Button trigger modal -->
+										  <h4 class="title">Profession Libérale</h4>
+
+
+									 <ul>
+											<li>
+											<h6><a href="#" data-toggle="modal" data-target="#activiteMedicale">Professionnels de santé</a></h6>
+											</li>
+											<li>
+											<h6><a href="#" data-toggle="modal" data-target="#activiteJustice">Professionnels de justice</a></h6>
+											</li>
+											<li>
+											<h6><a href="#" data-toggle="modal" data-target="#activiteNonReglemente">Activités libérales non réglementées</a></h6>
+											</li>
+
+								
+									</ul>
+							</div>
+			
+			
+					</div>
+		
+		
+
+		
+
+		
+          <div class="col-md-6 col-lg-2 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class=""></i></div>
+              <h4 class="title">Artisans</h4>
+					 <ul>
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteAlimentation">Artisan alimentaire</a></h6>
+					</li>
+
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteBTP">Batiments et Travaux Publics</a></h6>
+					</li>
+					
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteFabrication">Fabrication</a></h6>
+					</li>
+					
+					
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteAutreArtisan">Prestation de service artisanal </a>
+					</li>
+
+
+					</ul>
+            </div>
+          </div>
+		  
+
+
+          <div class="col-md-6 col-lg-2 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class=""></i></div>
+              <h4 class="title">Commerçants</h4>
+             <ul>
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteRestaurant">Restaurants</a></h6>
+					</li>
+					
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteHotel">Hotels</a></h6>
+					</li>
+					
+					
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteCommerceDetail">Commerce</a></h6>
+					</li>
+					
+
+				
+				</ul>
+            </div>
+          </div>
+		  
+		  
+		  		  <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class=""></i></div>
+              <h4 class="title">Activités Immobilières</h4>
+					 <ul>
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteSCI">Société civile immobilière (SCI)</a></h6>
+					</li>
+
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteLMNP">Loueur Meublé Non Professionnels (LMNP)</a></h6>
+					</li>
+					
+					
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteAgenceImmo">Agence Immobilière</a></h6>
+					</li>
+
+
+					</ul>
+            </div>
+          </div>
+		  
+		  
+
+          <div class="col-md-6 col-lg-2 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class=""></i></div>
+              <h4 class="title">Association</h4>
+					<ul>
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteAssociation">Association loi de 1901</a></h6>
+					</li>
+					<li>
+					<h6><a href="#" data-toggle="modal" data-target="#activiteONG">Organisation Non Gouvernementale</a></h6>
+					</li>
+					
+					</ul>
+            </div>
+          </div>
+
+        
+            </div>
+          </div>
+		  
+		  
+
+        </div><!-- End Row -->
+		
+		
+					</div><!--  end swiper-wrapper-->
+							<div class="swiper-pagination"></div>
+							
+			 
+			 </div><!-- End slider  -->
+		
+
+      </div><!-- End container -->
+	  
+	  
+    </section><!-- End Services Section -->
+	
+	
+	
+    <!-- ======= Including the modals pages ======= -->
+	<?php include "assets/html/liberal/modalSante.html" ?>
+	<?php include "assets/html/liberal/modalJustice.html" ?>
+	<?php include "assets/html/liberal/modalNonReglemente.html" ?>
+
+	<?php include "assets/html/artisan/modalAlimentation.html" ?>
+	<?php include "assets/html/artisan/modalBTP.html" ?>
+	<?php include "assets/html/artisan/modalFabrication.html" ?>
+	<?php include "assets/html/artisan/modalAutreArtisan.html" ?>
+    <?php include "assets/html/commerce/modalRestaurant.html" ?>
+	<?php include "assets/html/commerce/modalHotel.html" ?>
+	<?php include "assets/html/commerce/modalCommerceDetail.html" ?>
+	<?php include "assets/html/commerce/modalAutresCommercant.html" ?>
+ 	<?php include "assets/html/immobilier/modalSCI.html" ?>
+ 	<?php include "assets/html/immobilier/modalAgenceImmo.html" ?>
+ 	<?php include "assets/html/immobilier/modalLMNP.html" ?>
+	<?php include "assets/html/association/modalAssociation.html" ?>
+ 	<?php include "assets/html/association/modalONG.html" ?>
+ 	
+
+	
+	
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          
+          <p>Contactez-nous par téléphone, par email ou par le biais du formulaire ci-dessous</p>
+        </div>
+
+        
+
+        <div class="row mt-5">
+
+          <div class="col-lg-4">
+            <div class="info">
+			
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Adresse:</h4>
+                <p><a href="https://goo.gl/maps/N1mB6wrMVak5HbhE8" target="blank" >11, Place Général de Gaulle, 13001 Marseille</a></p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p><a href="mailto:contact@auditeurs-associes-marseille.fr">contact@auditeurs-associes-marseille.fr</a></p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Telephone :</h4>
+                <p><a href="tel:+33491599220">04 91 59 92 20 </a></p>
+              </div>
+
+				<div class="phone">
+                <i class="bi bi-clock"></i>
+                <h4>Horaires d’ouverture du cabinet :</h4>
+                <p><br />
+					Du lundi au vendredi <br />
+					De 9h00 à 12h30 et de 14H00 à 18h00</p>
+				</div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-8 mt-5 mt-lg-0">
+		  
+		  
+		  
+		  
+		  
+		  
+
+            <form method="post" class="contact_form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" class="form-control" id="visitor_name" name="visitor_name" placeholder="Nom et Prénom" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="visitor_email" id="visitor_email" placeholder="Adresse e-mail" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="email_title" id="email_title" placeholder="Objet" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="visitor_message" rows="5" placeholder="Message" required></textarea>
+              </div>
+
+				<div class="form-group mt-3">
+				
+					<div id="conteneur_bouton">
+              <button id="envoyer" type="submit">Envoyer</button>
+			  
+			  
+
+			
+	    <?php
+				if (isset($_POST['visitor_message'])) {
+					$entete  = 'MIME-Version: 1.0' . "\r\n";
+					$entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+					$entete .= 'From: contact@auditeurs-associes-marseille.fr' . "\r\n";
+					$entete .= 'Reply-to: ' . $_POST['visitor_email']. "\r\n";
+					$entete .= 'Cc: myboss@example.com';
+
+					$message = '<h2>Message envoyé de '. $_POST['visitor_name'] .' depuis le site www.auditeurs-associes-marseille.com</h2>
+					<p>
+					<b>Nom : </b>'. $_POST['visitor_name'] . '<br>
+					<b>Email : </b>' . $_POST['visitor_email'] . '<br>
+					<b>Objet : </b>'. $_POST['email_title'] . '<br>
+					<b>Message : </b> <br> ' . htmlspecialchars($_POST['visitor_message']) . '</p>';
+
+					$retour = mail('hajarasata@gmail.com', 'Vous avez un nouveau message reçu depuis le site www.auditeurs-associes-marseille.com ', $message, $entete);
+					if($retour)
+						
+					?>
+					
+					  <script>
+					  
+						alert ("Votre message a bien été envoyé");
+					  
+					  </script>
+					<?php
+					
+					
+						}
+						?>
+			
+								</div>
+			  </div>
+			  
+			  
+            </form>
+			
+			
+			
+			
+
+          </div>
+		  
+		  
+		  
+		  
+
+        </div>
+		
+		
+		<div class="myMap">
+          <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.931519112426!2d5.374222515354103!3d43.29475577913524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c0b92a032917%3A0x9e86790375f52639!2sAUDITEURS%20ASSOCIES%20FIDUCIAIRE%20CONSEILS!5e0!3m2!1sfr!2sfr!4v1648547699453!5m2!1sfr!2sfr" frameborder="0" allowfullscreen></iframe>
+        </div>
+		
+		
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+         
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Liens utiles</h4>
+            <ul>
+  
+              <li><i class="bx bx-chevron-right"></i> <a href="mentions.html">Mentions légales</a></li>
+			  
+			  <li><i class="bx bx-chevron-right"></i> <a href="https://annuaire.experts-comptables.org/expert-comptable/11029-auditeurs-associes-fiduciaire-conseils-marseille-13001" target="blank">Ordre des experts-comptables</a></li>
+			  <li><i class="bx bx-chevron-right"></i> <a href="https://www.urssaf.fr/portail/home.html" target="blank">Urssaf.fr</a></li>
+			  <li><i class="bx bx-chevron-right"></i> <a href="https://www.impots.gouv.fr/professionnel" target="blank">Impots.gouv.fr</a></li>
+            </ul>
+          </div>
+
+             <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Nos Partenaires</h4>
+            <ul>
+
+			 
+			  <li><i class="bx bx-chevron-right"></i> <a href="http://www.inatec-oi.com/" target="blank">INATEC-OI</a></li>
+			  <li><i class="bx bx-chevron-right"></i> <a href="mailto:hajarasata@gmail.com" target="blank">Webmaster</a></li>
+            </ul>
+          </div> 
+
+
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+			&copy; Auditeurs Associés Fiduciaire Conseils <script>document.write(new Date().getFullYear())</script>
+        </div>
+        <div class="credits">
+
+        </div>
+      </div>
+	  
+	        <div class="social-links text-center text-md-right pt-3 pt-md-0">
+				<a href="tel:+33491599220" class="phone"><i class="bi bi-telephone"></i></a>
+				<a href="mailto:contact@auditeurs-associes-marseille.fr" class="mail"><i class="bi bi-envelope"></i></a>
+				<a href="https://goo.gl/maps/N1mB6wrMVak5HbhE8" target="blank" class="map"><i class="bi bi-geo-alt"></i></a>
+			 </div>
+      
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  
+
+
+
+	<!-- Inclure les fichiers JavaScript de Bootstrap rajouté-->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+
+
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
